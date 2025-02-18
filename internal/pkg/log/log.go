f87051c10d661063317eea1c9dd5f54f66135445
xavier <me@zxavier.com>
2025-02-19 00:06:27 +0800

    替换 zlog 为 slog

--- internal/pkg/log/log.go
+++ internal/pkg/log/log.go
@@ -2,6 +2,8 @@ package log
 
 import (
 	"context"
+	"log/slog"
+	"os"
 	"sync"
 	"time"
 
@@ -28,13 +30,11 @@ type Logger interface {
 	Sync()
 }
 
-// zapLogger 是 Logger 接口的具体实现. 它底层封装了 zap.Logger.
-type zapLogger struct {
-	z *zap.Logger
+type sLogger struct {
+	s *slog.Logger
 }
 
-// 确保 zapLogger 实现了 Logger 接口. 以下变量赋值，可以使错误在编译期被发现.
-var _ Logger = &zapLogger{}
+var _ Logger = &sLogger{}
 
 var (
 	mu sync.Mutex
@@ -52,60 +52,26 @@ func Init(opts *Options) {
 }
 
 // NewLogger 根据传入的 opts 创建 Logger.
-func NewLogger(opts *Options) *zapLogger {
+func NewLogger(opts *Options) *sLogger {
 	if opts == nil {
 		opts = NewOptions()
 	}
 
-	// 将文本格式的日志级别，例如 info 转换为 zapcore.Level 类型以供后面使用
-	var zapLevel zapcore.Level
-	if err := zapLevel.UnmarshalText([]byte(opts.Level)); err != nil {
+	// 将文本格式的日志级别，例如 info 转换为 slog.Level 类型以供后面使用
+	var sLevel slog.Level
+	if err := sLevel.UnmarshalText([]byte(opts.Level)); err != nil {
 		// 如果指定了非法的日志级别，则默认使用 info 级别
-		zapLevel = zapcore.InfoLevel
+		sLevel = slog.LevelInfo
 	}
 
-	// 创建一个默认的 encoder 配置
-	encoderConfig := zap.NewProductionEncoderConfig()
-	// 自定义 MessageKey 为 message，message 语义更明确
-	encoderConfig.MessageKey = "message"
-	// 自定义 TimeKey 为 timestamp，timestamp 语义更明确
-	encoderConfig.TimeKey = "timestamp"
-	// 指定时间序列化函数，将时间序列化为 `2006-01-02 15:04:05.000` 格式，更易读
-	encoderConfig.EncodeTime = func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
-		enc.AppendString(t.Format("2006-01-02 15:04:05.000"))
-	}
-	// 指定 time.Duration 序列化函数，将 time.Duration 序列化为经过的毫秒数的浮点数
-	// 毫秒数比默认的秒数更精确
-	encoderConfig.EncodeDuration = func(d time.Duration, enc zapcore.PrimitiveArrayEncoder) {
-		enc.AppendFloat64(float64(d) / float64(time.Millisecond))
-	}
+	// 使用 cfg 创建 *slog.Logger 对象
+	s := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource: false,
+		Level:     sLevel,
+	}))
+	slog.SetDefault(s)
 
-	// 创建构建 zap.Logger 需要的配置
-	cfg := &zap.Config{
-		// 是否在日志中显示调用日志所在的文件和行号，例如：`"caller":"miniblog/miniblog.go:75"`
-		DisableCaller: opts.DisableCaller,
-		// 是否禁止在 panic 及以上级别打印堆栈信息
-		DisableStacktrace: opts.DisableStacktrace,
-		// 指定日志级别
-		Level: zap.NewAtomicLevelAt(zapLevel),
-		// 指定日志显示格式，可选值：console, json
-		Encoding:      opts.Format,
-		EncoderConfig: encoderConfig,
-		// 指定日志输出位置
-		OutputPaths: opts.OutputPaths,
-		// 设置 zap 内部错误输出位置
-		ErrorOutputPaths: []string{"stderr"},
-	}
-
-	// 使用 cfg 创建 *zap.Logger 对象
-	z, err := cfg.Build(zap.AddStacktrace(zapcore.PanicLevel), zap.AddCallerSkip(1))
-	if err != nil {
-		panic(err)
-	}
-	logger := &zapLogger{z: z}
-
-	// 把标准库的 log.Logger 的 info 级别的输出重定向到 zap.Logger
-	zap.RedirectStdLog(z)
+	logger := &sLogger{s}
 
 	return logger
 }
@@ -220,17 +186,15 @@ func (l *zapLogger) Fatalw(msg string, keysAndValues ...interface{}) {
 }
 
 // C 解析传入的 context，尝试提取关注的键值，并添加到 zap.Logger 结构化日志中.
-func C(ctx context.Context) *zapLogger {
+func C(ctx context.Context) *sLogger {
 	return std.C(ctx)
 }
 
-func (l *zapLogger) C(ctx context.Context) *zapLogger {
-	lc := l.clone()
-
+func (l *sLogger) C(ctx context.Context) *sLogger {
 	if requestID := ctx.Value(common.XRequestIDKey); requestID != nil {
 		lc.z = lc.z.With(zap.Any(common.XRequestIDKey, requestID))
 	}
-	return lc
+	return l.s.WarnContext()
 }
 
 // clone 深度拷贝 zapLogger.
