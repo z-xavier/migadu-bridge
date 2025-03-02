00b5b3b9e139cbf78a3f42583040cf737af1beb7
simona <laimengsha@qq.com>
2025-03-02 21:04:05 +0800

    remove frontend-next

--- frontend-next/src/pages/alias/index.tsx
+++
@@ -1,5 +0,0 @@
-import Layout from '../../components/layout'
-
-export default function AliasPage() {
-  return <Layout></Layout>
-}
--- frontend-next/src/pages/log/index.tsx
+++
@@ -1,5 +0,0 @@
-import Layout from '../../components/layout'
-
-export default function LogPage() {
-  return <Layout></Layout>
-}
